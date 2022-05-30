393299a17ac064ad5c9f549922b3f254f73cf451
zyysehun <1620544943@qq.com>
2022-05-30 11:30:43 +0800

    完成了分类参数的开发

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Users from '@/components/user/users.vue'
 import Rights from '@/components/power/Right.vue'
 import Roles from '@/components/power/Roles.vue'
 import Cate from '@/components/goods/Cate.vue'
+import Params from '@/components/goods/Params.vue'
 
 Vue.use(VueRouter)
 
@@ -28,7 +29,8 @@ const router = new VueRouter({
         { path: '/users', component: Users },
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
-        { path: '/categories', component: Cate }
+        { path: '/categories', component: Cate },
+        { path: '/params', component: Params }
       ]
     }
   ]
